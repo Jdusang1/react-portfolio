bdaa2da2e7bdd58d86a1d19b485963209e693fa2
JohnDusang <Johndusang1@gmail.com>
2020-09-01 00:19:36 -0600

    Updates

---
+++ precache-manifest.0794cb6dd05c43919e6f80af2d411484.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f673923784917d702264d15d7736fd",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0ccd471f6b33bf94a755",
+    "url": "/react-portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5f8ff1edb44a86eb7ce7",
+    "url": "/react-portfolio/static/css/main.734cbedc.chunk.css"
+  },
+  {
+    "revision": "0ccd471f6b33bf94a755",
+    "url": "/react-portfolio/static/js/2.5dae4ec0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.5dae4ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f8ff1edb44a86eb7ce7",
+    "url": "/react-portfolio/static/js/main.6a7dfde6.chunk.js"
+  },
+  {
+    "revision": "a05a49d9351ba3961537",
+    "url": "/react-portfolio/static/js/runtime-main.bc59954b.js"
+  },
+  {
+    "revision": "152277c47a75933337d06390fa4f3aeb",
+    "url": "/react-portfolio/static/media/Campfound.152277c4.jpg"
+  },
+  {
+    "revision": "627e409c07cb802abf6006d7f30fc870",
+    "url": "/react-portfolio/static/media/IMG_3927.627e409c.JPG"
+  },
+  {
+    "revision": "de90748b705acc0f76bbe66fc79ed5a5",
+    "url": "/react-portfolio/static/media/IMG_6917.de90748b.JPG"
+  },
+  {
+    "revision": "df21b61c0142eeb7a003545b9ea59788",
+    "url": "/react-portfolio/static/media/IMG_9025.df21b61c.JPG"
+  },
+  {
+    "revision": "8eb2ecd8edd39343e8144d00dc398d05",
+    "url": "/react-portfolio/static/media/We-Park .8eb2ecd8.jpg"
+  },
+  {
+    "revision": "61897658d54d5ebb44f8daebb4e15290",
+    "url": "/react-portfolio/static/media/Worthy-Origin.61897658.jpg"
+  }
+]);
